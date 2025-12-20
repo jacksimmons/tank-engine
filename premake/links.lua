c1bdc37ef176b9bef803f01f68047f68b4254b45
Jack Simmons <56202789+jacksimmons@users.noreply.github.com>
2025-12-20 09:19:21 +0000

    Added apt-packages.txt

--- premake/links.lua
+++ premake/links.lua
@@ -37,7 +37,7 @@ function LinkGLFW()
 	filter { "system:windows" }
 		links { "glfw3" }
 	filter { "system:not windows" }
-		links { "glfw" } -- libglfw.so.3 is named "glfw"
+		links { "glfw" } -- libglfw.so.3 has name "glfw"
 	filter {}
 end
 
