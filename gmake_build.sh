e7e4169dfbfaa5a0fbad95f9ce32e005c4a05b29
Jack Simmons <jack-simmons@outlook.com>
2026-03-28 16:38:05 +0000

    Fixed PCH linting for Ubuntu

--- gmake_build.sh
+++ gmake_build.sh
@@ -1,8 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 premake/premake5 gmake
 
-# First param = Clean
-# if [ $# -ne 0 ]; then
-    # make clean
-# fi
-make all CFLAGS="-fPIC"
+# Clean build
+# Only generate compile_commands.json if a clean build was requested
+if [[ "$1" = "--clean" || "$1" = "-c" ]]; then
+    make clean
+    bear -- make all CFLAGS="-fPIC"
+fi
+
+perl -i -0pe 's|("-include",\s*")[^"]*tepch\.hpp"|$1'"$(pwd)"'/TankEngine/tepch.hpp"|g' compile_commands.json
