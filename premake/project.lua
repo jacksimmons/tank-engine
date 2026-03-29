a0108e3d1e548c2b6979f5178be32bb6b8b80375
Jack Simmons <jack-simmons@outlook.com>
2026-03-29 17:26:32 +0100

    Add comments

--- premake/project.lua
+++ premake/project.lua
@@ -1,25 +1,27 @@
 local subDir = "%{prj.name}/%{cfg.shortname}"
 local groupSubDir = "%{prj.group}/%{prj.name}/%{cfg.shortname}"
 
+---comment Sets project language to c
 function PrjUseC()
     language "C"
     cdialect "c17"
 end
 
-
+---comment Sets project language to c++
 function PrjUseCpp()
     language "C++"
 	cppdialect "c++23"
 end
 
+---comment Sets project language to C#
 function PrjUseCSharp()
     language "C#"
     dotnetframework "net10.0"
 end
 
 function PrjObjAndTargetDir()
-    targetdir ("Builds/bin/" .. subDir)
-	objdir ("Builds/obj/" .. subDir)
+    targetdir ("%{wks.location}/Builds/bin/" .. subDir)
+	objdir ("%{wks.location}/Builds/obj/" .. subDir)
 end
 
 function GrpPrjObjAndTargetDir()
